1a138e9bbc8ef4f6d5e5b4062b492be98455ec5e
lizhen <lizhen0211@163.com>
2019-08-21 15:53:58 +0800

    baseline

--- app/src/main/java/com/lz/android_rxjava2_sample/SimpleDemoActivity.java
+++ app/src/main/java/com/lz/android_rxjava2_sample/SimpleDemoActivity.java
@@ -125,7 +125,8 @@ public class SimpleDemoActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     //上游发送了一个onError后, 上游onError之后的事件将继续发送
                     //而下游收到onError事件之后将不再继续接收事件
-                    emitter.onError(e);
+
+                    //emitter.onError(e);
                     e.printStackTrace();
                 }
                 //上游可以不发送onComplete或onError.
@@ -140,6 +141,56 @@ public class SimpleDemoActivity extends AppCompatActivity {
             @Override
             public void onNext(Integer value) {
                 Log.e(TAG, "" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "complete");
+            }
+        });
+    }
+
+    /**
+     * 如果把Observable 理解为 上游水管，把Observable理解为下游水管
+     * subscribe 是把两根水管连在一起，dispose则是将两根水管断开
+     * 调用dispose()并不会导致上游不再继续发送事件， 上游会继续发送剩余的事件，但下游收不到事件。
+     *
+     * @param view
+     */
+    public void onDisposeClick(View view) {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                Log.e(TAG, "emit 1");
+                emitter.onNext(1);
+                Log.e(TAG, "emit 2");
+                emitter.onNext(2);
+                Log.e(TAG, "emit 3");
+                emitter.onNext(3);
+                Log.e(TAG, "emit complete");
+                emitter.onComplete();
+                Log.e(TAG, "emit 4");
+                emitter.onNext(4);
+            }
+        }).subscribe(new Observer<Integer>() {
+            private Disposable mDisposable;
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, "subscribe");
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.e(TAG, "onNext: " + value);
+                mDisposable.dispose();
+                Log.e(TAG, "isDisposed : " + mDisposable.isDisposed());
             }
 
             @Override
