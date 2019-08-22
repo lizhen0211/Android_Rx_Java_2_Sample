a84aa7a48a765371cce36043d5ebd30ab3424a43
lizhen <lizhen0211@163.com>
2019-08-22 13:41:12 +0800

    baseline

--- app/src/main/java/com/lz/android_rxjava2_sample/OperatorActivity.java
+++ app/src/main/java/com/lz/android_rxjava2_sample/OperatorActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 
@@ -22,6 +27,8 @@ public class OperatorActivity extends AppCompatActivity {
     }
 
     /**
+     * map 操作符
+     * <p>
      * 对上游发送的每一个事件应用一个函数
      * 将上游接口数据，转换成下游接口需要的数据
      *
@@ -40,6 +47,74 @@ public class OperatorActivity extends AppCompatActivity {
             public String apply(Integer integer) throws Exception {
                 //这里将Integer 转换成了 String
                 return "This is result " + integer;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.e(TAG, s);
+            }
+        });
+    }
+
+    /**
+     * flatMap 操作符
+     * <p>
+     * 将一个发送事件的上游Observable变换为多个发送事件的Observables
+     * 然后将它们发射的事件合并后放进一个单独的Observable里
+     *
+     * @param view
+     */
+    public void onFlatMapOperatorClick(View view) {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+            }
+        }).flatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Integer integer) throws Exception {
+                final List<String> list = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                //此处将上游的一个Observable 转换成了一组Observable(新创建)
+                return Observable.fromIterable(list).delay(10, TimeUnit.MILLISECONDS);
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.e(TAG, s);
+            }
+        });
+    }
+
+    /**
+     * concatMap 操作符
+     * <p>
+     * 注意: flatMap并不保证事件的顺序，并不是事件1就在事件2的前面.
+     * concatMap 可以保证事件的顺序 结果是严格按照上游发送的顺序来发送
+     *
+     * @param view
+     */
+    public void onConcatMapOperatorClick(View view) {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+            }
+        }).concatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Integer integer) throws Exception {
+                final List<String> list = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                //此处将上游的一个Observable 转换成了一组Observable(新创建)
+                return Observable.fromIterable(list).delay(10, TimeUnit.MILLISECONDS);
             }
         }).subscribe(new Consumer<String>() {
             @Override
