12fb7e264656d5c0375bde44652aa69099abaeb4
lizhen <lizhen0211@163.com>
2019-08-23 14:49:35 +0800

    baseline

--- app/src/main/java/com/lz/android_rxjava2_sample/BackpressureActivity.java
+++ app/src/main/java/com/lz/android_rxjava2_sample/BackpressureActivity.java
@@ -1,7 +1,7 @@
 package com.lz.android_rxjava2_sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
@@ -12,6 +12,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class BackpressureActivity extends AppCompatActivity {
@@ -69,5 +70,79 @@ public class BackpressureActivity extends AppCompatActivity {
 
         //没有被处理的消息会储到 queue 中。
         //看profiler 内存一直在增加。
+        //注：出现上游（生产者）发送 速度 大于 下游（消费者）发送速度，就会出现这种问题。
+        //最终导致OOM
+    }
+
+    /**
+     * 通过 减少上游发送量 解决背压
+     *
+     * @param view
+     */
+    public void onSolveBackjianpressure1(View view) {
+        //使用filter操作符
+        Disposable disposable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; ; i++) {
+                    emitter.onNext(i);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        //只有被10整出的事件才发送
+                        return integer % 10 == 0;
+                    }
+                }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "" + integer);
+                    }
+                });
+        //使用sample操作符
+        /*Disposable disposable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; ; i++) {
+                    emitter.onNext(i);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .sample(2, TimeUnit.SECONDS)  //sample取样，每隔2秒取一个事件给下游
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "" + integer);
+                    }
+                });*/
+    }
+
+    /**
+     * 通过 减慢上游发送速度 解决背压
+     *
+     * @param view
+     */
+    public void onSolveBackpressure2(View view) {
+        Disposable disposable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; ; i++) {
+                    emitter.onNext(i);
+                    //每次发送完事件延时2秒
+                    Thread.sleep(2000);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "" + integer);
+                    }
+                });
     }
 }
