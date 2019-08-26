a11c3f593daf890b5b211b834996060dc8769ef2
lizhen <lizhen0211@163.com>
2019-08-26 11:15:59 +0800

    baseline

--- app/src/main/java/com/lz/android_rxjava2_sample/FlowableActivity.java
+++ app/src/main/java/com/lz/android_rxjava2_sample/FlowableActivity.java
@@ -12,6 +12,8 @@ import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class FlowableActivity extends AppCompatActivity {
 
@@ -23,32 +25,39 @@ public class FlowableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_flowable);
     }
 
+    /**
+     * 上游和下游在同一个线程中执行
+     *
+     * @param view
+     */
     public void onTestFlowableDemo1Click(View view) {
         Flowable<Integer> upstream = Flowable.create(new FlowableOnSubscribe<Integer>() {
             @Override
             public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
-                Log.d(TAG, "emit 1");
+                Log.e(TAG, "emit 1");
                 emitter.onNext(1);
-                Log.d(TAG, "emit 2");
+                Log.e(TAG, "emit 2");
                 emitter.onNext(2);
-                Log.d(TAG, "emit 3");
+                Log.e(TAG, "emit 3");
                 emitter.onNext(3);
-                Log.d(TAG, "emit complete");
+                Log.e(TAG, "emit complete");
                 emitter.onComplete();
             }
-        }, BackpressureStrategy.ERROR); //增加了一个参数
+        }, BackpressureStrategy.ERROR); //增加被压策略
 
         Subscriber<Integer> downstream = new Subscriber<Integer>() {
 
             @Override
             public void onSubscribe(Subscription s) {
-                Log.d(TAG, "onSubscribe");
+                Log.e(TAG, "onSubscribe");
                 s.request(Long.MAX_VALUE);
+                //这里去掉会抛异常
+                //io.reactivex.exceptions.MissingBackpressureException: create: could not emit value due to lack of requests
             }
 
             @Override
             public void onNext(Integer integer) {
-                Log.d(TAG, "onNext: " + integer);
+                Log.e(TAG, "onNext: " + integer);
 
             }
 
@@ -59,11 +68,149 @@ public class FlowableActivity extends AppCompatActivity {
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete");
+                Log.e(TAG, "onComplete");
             }
         };
 
         upstream.subscribe(downstream);
+    }
+
+    /**
+     * 上游和下游在同不同的线程中执行
+     *
+     * @param view
+     */
+    public void onTestFlowableDemo2Click(View view) {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                Log.e(TAG, "emit 1");
+                emitter.onNext(1);
+                Log.e(TAG, "emit 2");
+                emitter.onNext(2);
+                Log.e(TAG, "emit 3");
+                emitter.onNext(3);
+                Log.e(TAG, "emit complete");
+                emitter.onComplete();
+            }
+        }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    Subscription mSubscription;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe");
+                        mSubscription = s;
+                        //这里没有调用 Subscription 的 request方法
+                        //下游没有接收到任何事件
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.e(TAG, "onError: ", t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete");
+                    }
+                });
+    }
+
+    private Subscription mSubscription;
+
+    /**
+     * Flowable 的设计方式 为 响应式拉取
+     * request 表示下游处理事件的能力
+     * Subscription.request(1) 表示拉取一个
+     * 下游能处理几个事件就拉取几个事件.
+     *
+     * @param view
+     */
+    public void onTestFlowableDemo3Click(View view) {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                Log.e(TAG, "emit 1");
+                emitter.onNext(1);
+                Log.e(TAG, "emit 2");
+                emitter.onNext(2);
+                Log.e(TAG, "emit 3");
+                emitter.onNext(3);
+                Log.e(TAG, "emit complete");
+                emitter.onComplete();
+            }
+        }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe");
+                        mSubscription = s;  //把Subscription保存起来
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.w(TAG, "onError: ", t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete");
+                    }
+                });
+    }
+
+    public void onDemo3RequestClick(View view) {
+        mSubscription.request(1);
+    }
+
+    public void onTestFlowableDemo4Click(View view) {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                //此处不能超过128，因为 Flowable 的 BUFFER_SIZE 为 128，超过会溢出
+                for (int i = 0; i < 128; i++) {
+                    Log.e(TAG, "emit " + i);
+                    emitter.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe");
+                        s.request(Integer.MAX_VALUE);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.e(TAG, "onError: ", t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete");
+                    }
+                });
     }
 
 }
