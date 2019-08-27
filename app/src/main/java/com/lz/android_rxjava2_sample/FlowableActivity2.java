0c37e8675c510e83a93c665c7b3465c0a37751b5
lizhen <lizhen0211@163.com>
2019-08-27 15:40:20 +0800

    baseline

--- app/src/main/java/com/lz/android_rxjava2_sample/FlowableActivity2.java
+++ app/src/main/java/com/lz/android_rxjava2_sample/FlowableActivity2.java
@@ -17,6 +17,15 @@ import io.reactivex.schedulers.Schedulers;
 
 /**
  * BackpressureStrategy
+ * <p>
+ * 背压问题的出现需要两个条件：
+ * 1、上游与下游需处于不同线程。
+ * 2、被观察者产生事件的速度需远快与观察者消费事件的速度。
+ * <p>
+ * 注：
+ * 背压策略的解决思路是响应式拉取。
+ * 与RxJava观察者模型相反，响应式拉取是观察者主动去被观察者那里拉取事件，
+ * 而被观察者则是被动等待通知再发射事件
  */
 public class FlowableActivity2 extends AppCompatActivity {
 
@@ -28,40 +37,128 @@ public class FlowableActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_flowable2);
     }
 
+    private Subscription bufferSubscription;
+
     public void onBufferStrategyClick(View view) {
         Flowable.create(new FlowableOnSubscribe<Integer>() {
             @Override
             public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
                 for (int i = 0; i < 1000; i++) {
-                    Log.d(TAG, "emit " + i);
+                    Log.e(TAG, "emit " + i);
                     emitter.onNext(i);
                 }
             }
-            //BackpressureStrategy.DROP
-            //BackpressureStrategy.LATEST
         }, BackpressureStrategy.BUFFER).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe");
+                        bufferSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.e(TAG, "onError: ", t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete");
+                    }
+                });
+    }
+
+    public void onBufferRequestClick(View view) {
+        bufferSubscription.request(228);
+    }
+
+    private Subscription dropSubscription;
+
+    public void onDropStrategyClick(View view) {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; i < 10000; i++) {
+                    Log.e(TAG, "emit " + i);
+                    emitter.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.DROP).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<Integer>() {
 
                     @Override
                     public void onSubscribe(Subscription s) {
-                        Log.d(TAG, "onSubscribe");
+                        Log.e(TAG, "onSubscribe");
+                        dropSubscription = s;
                     }
 
                     @Override
                     public void onNext(Integer integer) {
-                        Log.d(TAG, "onNext: " + integer);
+                        Log.e(TAG, "onNext: " + integer);
                     }
 
                     @Override
                     public void onError(Throwable t) {
-                        Log.w(TAG, "onError: ", t);
+                        Log.e(TAG, "onError: ", t);
                     }
 
                     @Override
                     public void onComplete() {
-                        Log.d(TAG, "onComplete");
+                        Log.e(TAG, "onComplete");
                     }
                 });
+    }
+
+    public void onDropRequestClick(View view) {
+        dropSubscription.request(228);
+    }
+
+    private Subscription latestSubscription;
+
+    public void onLatestStrategyClick(View view) {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; i < 10000; i++) {
+                    Log.e(TAG, "emit " + i);
+                    emitter.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.LATEST).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe");
+                        latestSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.e(TAG, "onError: ", t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete");
+                    }
+                });
+    }
+
+    public void onLatestRequestClick(View view) {
+        latestSubscription.request(228);
     }
 }
